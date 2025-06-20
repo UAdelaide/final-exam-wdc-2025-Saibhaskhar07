f7080c539c6ed8af55096caf05ccf23d25d9fec4
“Saibhaskhar <saibhaskharr@gmail.com>
2025-06-20 07:53:22 +0000

    [automatic commit] 2025-06-20 07:53:22 UTC 129.127.204.51

--- part1/app.js
+++ part1/app.js
@@ -154,7 +154,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 
 app.get('/api/walkers/summary', async (req, res) => {
   try {
-    const [WalkerSummary] = await db.execute('SELECT Users.username, COUNT(WalkRatings.rating_id, AVG(WalkRatings.rating),COUNT(CASE WHEN WalkRequest.status = "Completed" AND WalkApplication.status = "Accepted" THEN WalkRequest.request_id END) FROM Users LEFT JOIN WalkRatings On Users.user_id = WalkRatings.walker_id LEFT JOIN WalkApplications ON Users.user_id = WalkApplication.walker_id LEFT JOIN WalkRequests ON WalkApplication.request_id');
+    const [WalkerSummary] = await db.execute('SELECT Users.username, COUNT(WalkRatings.rating_id, AVG(WalkRatings.rating),COUNT(CASE WHEN WalkRequest.status = "Completed" AND WalkApplication.status = "Accepted" THEN WalkRequest.request_id END) FROM Users LEFT JOIN WalkRatings On Users.user_id = WalkRatings.walker_id LEFT JOIN WalkApplications ON Users.user_id = WalkApplication.walker_id LEFT JOIN WalkRequests ON WalkApplication.request_id = WalkRequests');
     res.json(WalkerSummary);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch Walker Summary' });
