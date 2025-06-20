035b550a0bb78b46596e707c6153fec200c707c6
“Saibhaskhar <saibhaskharr@gmail.com>
2025-06-20 14:54:23 +0000

    [automatic commit] 2025-06-20 14:54:23 UTC 129.127.204.51

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -81,7 +81,8 @@ router.post('/dogs',(req,res) => {
   try{
     const [rows] = await db.query(`SELECT dog_id, name FROM Dogs WHERE owner_id = ? `,[owner_id]) ;
     res.json(rows);
-  }
+  } catch(error) {
+    res.status(500).json({ error: 'Login failed' });
 })
 
 module.exports = router;
