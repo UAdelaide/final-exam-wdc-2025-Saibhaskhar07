f6f9e67b504eaa00a6952936051fde341a2341d3
“Saibhaskhar <saibhaskharr@gmail.com>
2025-06-20 06:37:51 +0000

    [automatic commit] 2025-06-20 06:37:51 UTC 49.178.102.85

--- part1/app.js
+++ part1/app.js
@@ -76,12 +76,14 @@ let db ;
 
     await db.execute(`
       CREATE TABLE IF NOT EXISTS WalkApplications (
-        user_id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
-        email VARCHAR(100) UNIQUE NOT NULL,
-        password_hash VARCHAR(255) NOT NULL,
-        role ENUM('owner', 'walker') NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        application_id INT AUTO_INCREMENT PRIMARY KEY,
+        request_id INT NOT NULL,
+        walker_id INT NOT NULL,
+        applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+        FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
+        FOREIGN KEY (walker_id) REFERENCES Users(user_id),
+        CONSTRAINT unique_application UNIQUE (request_id, walker_id)
         )
     `);
 
