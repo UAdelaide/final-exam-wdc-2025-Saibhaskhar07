0680f4a679ce86521d149184e1c16715a9b633a8
“Saibhaskhar <saibhaskharr@gmail.com>
2025-06-20 13:52:34 +0000

    [automatic commit] 2025-06-20 13:52:34 UTC 129.127.204.51

--- part2/app.js
+++ part2/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-
+const session = require
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
@@ -14,6 +15,7 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+
 
 // Export the app instead of listening here
 module.exports = app;
