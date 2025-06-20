ac04608982a182bd1993dfe5c7c67cd2adc872e0
“Saibhaskhar <saibhaskharr@gmail.com>
2025-06-20 06:35:30 +0000

    [automatic commit] 2025-06-20 06:35:29 UTC 49.178.102.85

--- part1/app.js
+++ part1/app.js
@@ -46,9 +46,9 @@ let db ;
         username VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
         password_hash VARCHAR(255) NOT NULL,
-    role ENUM('owner', 'walker') NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+        role ENUM('owner', 'walker') NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
     `);
 
     // Insert data if table is empty
