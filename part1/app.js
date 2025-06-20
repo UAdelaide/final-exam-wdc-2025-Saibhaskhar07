d2bd9432f53b7b18b3cae1f99a78670eb9c0dff6
“Saibhaskhar <saibhaskharr@gmail.com>
2025-06-20 06:44:16 +0000

    [automatic commit] 2025-06-20 06:44:16 UTC 49.178.102.85

--- part1/app.js
+++ part1/app.js
@@ -106,9 +106,9 @@ let db ;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
     if (rows[0].count === 0) {
       await db.execute(`
-    INSERT INTO Users(username, email, password_hash, role) 
-    VALUES ('alice123','alice@example.com',' hashed123','owner'),
-    ('bobwalker','bob@example.com','hashed456','walker'),
+        INSERT INTO Users(username, email, password_hash, role) 
+        VALUES ('alice123','alice@example.com',' hashed123','owner'),
+        ('bobwalker','bob@example.com','hashed456','walker'),
     ('carol123','carol@example.com','hashed789','owner'),
     ('mackenzie589','kenize@gmail.com','hashed1010','owner'),
     ('kyle','kyle@gmail.com','hashed8797','walker') ; 
