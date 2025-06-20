f0f7a8158219f28ce7daa8a81cf117169aab1d70
“Saibhaskhar <saibhaskharr@gmail.com>
2025-06-20 07:04:32 +0000

    [automatic commit] 2025-06-20 07:04:32 UTC 49.178.102.85

--- part1/app.js
+++ part1/app.js
@@ -145,7 +145,7 @@ app.get('/api/Dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
-    const [Dogs] = await db.execute('SELECT * FROM Dogs');
+    const [Dogs] = await db.execute('SELECT ');
     res.json(Dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch Work requests' });
