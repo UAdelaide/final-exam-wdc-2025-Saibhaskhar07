16336f8872966ac3801b5904432f4c64083eaf27
“Saibhaskhar <saibhaskharr@gmail.com>
2025-06-20 16:01:07 +0000

    [automatic commit] 2025-06-20 16:01:07 UTC 129.127.204.51

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -90,7 +90,9 @@ router.get('/me', (req,res)=>{
   if(!req.session.user_id) {
     return res.status(401).json({error:'Not logged in '});
   }
-  res.json
+  res.json({
+    user_id: req.session.user_id
+  })
 })
 
 module.exports = router;
