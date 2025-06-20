834786c90b998d14ed1bf640a12eab2d5353cade
“Saibhaskhar <saibhaskharr@gmail.com>
2025-06-20 06:36:50 +0000

    [automatic commit] 2025-06-20 06:36:50 UTC 49.178.102.85

--- part1/app.js
+++ part1/app.js
@@ -62,7 +62,7 @@ let db ;
     `);
 
     await db.execute(`
-      CREATE TABLE IF NOT EXISTS Users (
+      CREATE TABLE IF NOT EXISTS WalkRequests (
         user_id INT AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(50) UNIQUE NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
