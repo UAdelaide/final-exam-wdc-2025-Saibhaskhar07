9933dbdf711ca66f1c213216fbcde28f54edb14e
“Saibhaskhar <saibhaskharr@gmail.com>
2025-06-21 01:51:55 +0000

    [automatic commit] 2025-06-21 01:51:55 UTC 122.199.53.102

--- part1/app.js
+++ part1/app.js
@@ -145,7 +145,7 @@ app.get('/api/Dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
-    const [WalkRequests] = await db.execute('SELECT WalkRequests.request_id, Dogs.name as dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS FROM WalkRequests INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE WalkRequests.Status = "open"')
+    const [WalkRequests] = await db.execute('SELECT WalkRequests.request_id, Dogs.name as dog_name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.username AS owner_username FROM WalkRequests INNER JOIN Dogs ON WalkRequests.dog_id = Dogs.dog_id INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE WalkRequests.Status = "open"')
     res.json(WalkRequests);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch Walk requests' });
